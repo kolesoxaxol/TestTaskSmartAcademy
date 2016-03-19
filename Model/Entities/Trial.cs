f0feb48c741d7de55c4936a307c18f7c16ab3be4
kolesoxaxol <kolesoxaxol@mail.ru>
2016-03-19 14:32:02 +0300

    create new application

---
+++ Model/Entities/Trial.cs
@@ -0,0 +1,20 @@
+﻿using Model.MetaEntitites;
+using System;
+
+namespace Model.Entities
+{
+    
+    public class Trial : IGenericModel
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public string StudyType { get; set; }
+
+        public string InterventionType { get; set; }
+        public int Phase { get; set; }
+    }
+}
